3cf7f90c1eb94331ff865da118ed3dbdd3754fd9
Ndisyamwende <sharonndisya2002@gmail.com>
2023-12-24 07:11:38 +0300

    commpleted assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
+function addingEventListeners() {
+document.addEventListener('click', function(event) {
+    alert('i was clicked!');
+  });
+  
+  const input = document.getElementById('input');
+  
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+  
+  input.addEventListener('click', clickAlert); 
 }
